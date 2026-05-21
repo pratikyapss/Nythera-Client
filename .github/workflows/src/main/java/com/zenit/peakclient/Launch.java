fa82ca2deef0ec69e1e21cd5f7d541a8e1fd1b95
pratikyapss <pray2382@gmail.com>
2026-05-21 18:35:36 +0530

    Add Launch class to start Peak Client

---
+++ .github/workflows/src/main/java/com/zenit/peakclient/Launch.java
@@ -0,0 +1,10 @@
+package com.zenit.peakclient;
+
+import net.minecraft.client.main.Main;
+
+public class Launch {
+    public static void main(String[] args) {
+        System.out.println("[PeakClient] Starting Peak Client by Zenit...");
+        Main.main(args);
+    }
+}
